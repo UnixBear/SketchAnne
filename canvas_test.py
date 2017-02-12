e800e972b9d00907b71ef2d213125a4e6f19bea1
adrayv <draym001@g.ucla.edu>
2017-02-11 16:34:13 -0800

    saving canvas_test

--- canvas_test.py
+++ canvas_test.py
@@ -7,9 +7,12 @@ canvas_height = 400
 
 def paint( event ):
    python_green = "#476042"
-   x1, y1 = ( event.x - 1 ), ( event.y - 1 )
+   x1, y1 = ( event.x - 0 ), ( event.y - 0 )
    x2, y2 = ( event.x + 1 ), ( event.y + 1 )
-   w.create_oval( x1, y1, x2, y2, fill = python_green )
+   w.create_line(x1, y1, x2, y2, dash=(4,2))
+   #w.create_oval( x1, y1, x2, y2, fill = python_green )
+def line(event):
+    w.create_line(300, 200, 500, 600)
 
 master = Tk()
 master.title( "Painting using Ovals" )
@@ -17,7 +20,8 @@ w = Canvas(master,
            width=canvas_width, 
            height=canvas_height)
 w.pack(expand = YES, fill = BOTH)
-w.bind( "<B1-Motion>", paint )
+#w.bind( "<B1-Motion>", paint )
+w.bind("<Button-1>", line)
 
 message = Label( master, text = "Press and Drag the mouse to draw" )
 message.pack( side = BOTTOM )
